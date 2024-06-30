1bd12ce1c88c0a4421d827a64836243dac309ba6
Lazizbek <oddiybolakay05@gmail.com>
2024-06-30 22:35:56 +0500

    branch to main

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () =>{
             <div className="flex justify-center md:justify-between align-middle flex-wrap gap-3 py-7 border-t">
                 <div className=""><h1 className="text-primary italic text-base font-normal leading-relaxed tracking-normal text-left">2016–2021 © ООО «ПД Реснота»</h1></div>
                 <div className="cursor-pointer"><img src={icons} alt="" /></div>
-                <div className=""><h1 className="text-primary italic text-base font-normal leading-relaxed tracking-normal text-left">Разработка и продвижение сайта — SEOabsolut</h1></div>
+                <div className=""><h1 className="text-primary italic text-base font-normal leading-relaxed tracking-normal text-center lg:text-left">Разработка и продвижение сайта — SEOabsolut</h1></div>
             </div>
         </div>
         </>
